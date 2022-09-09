372fa53f089b60589553faf2b81423869c76caba
ThriftShop <petermac0007@gmail.com>
2022-09-09 12:17:07 +0000

    fixed errors

--- models/deals_dbt.sql
+++ models/deals_dbt.sql
@@ -4,15 +4,16 @@ with transform_deals as (
 
  SELECT 
   ROW_NUMBER() OVER() AS deal_id,
-  product product_id,
+  product_id,
+  product_name,
   'half_price' as deal_type,
-  'Big Basket' as shop_name,
+  'bigbasket' as shop_name,
   current_datetime as deal_start_datetime,
   DATETIME_ADD(DATETIME "2022-09-07 00:00:00", INTERVAL 1 WEEK) as deal_end_datetime,
   '50 %' deal_percentage,
   (market_price * 0.5) as discounted_price,
   current_datetime as created_datetime
-  from `thriftshop_mysql_test.product_250`
+  from `thriftshop_mysql_test.product_staging`
   order by rand() limit 50
 )
 
