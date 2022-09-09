1a7c4e883c5b44d86c54d743a414ec0f96c1e425
ThriftShop <petermac0007@gmail.com>
2022-09-09 05:34:48 +0000

    Added random function

--- models/deals_dbt.sql
+++ models/deals_dbt.sql
@@ -6,13 +6,14 @@ with transform_deals as (
   ROW_NUMBER() OVER() AS deal_id,
   product product_id,
   'half_price' as deal_type,
-  'bigbasket' as shop_name,
+  'Big Basket' as shop_name,
   current_datetime as deal_start_datetime,
   DATETIME_ADD(DATETIME "2022-09-07 00:00:00", INTERVAL 1 WEEK) as deal_end_datetime,
   '50 %' deal_percentage,
   (market_price * 0.5) as discounted_price,
   current_datetime as created_datetime
   from `thriftshop_mysql_test.product_250`
+  order by rand() limit 50
 )
 
 select *
