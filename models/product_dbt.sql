b24dd88a873755fa551c76ca39a5aba5928efd83
ThriftShop <petermac0007@gmail.com>
2022-09-09 09:34:20 +0000

    changed percentage

--- models/product_dbt.sql
+++ models/product_dbt.sql
@@ -2,7 +2,7 @@
 
 with transform_product as (
 select
-  product_id
+  product_id	
   ,product_name		
   ,product_category		
   ,product_sub_category			
@@ -12,7 +12,7 @@ select
   ,product_type	
   ,product_rating			
   ,product_description	
-  ,round((market_price * 1.1), 2) as tax_percentage				
+  ,('10%') as tax_percentage				
   ,'https://www.istockphoto.com/illustrations/loading-icon' as product_image			
   ,current_datetime as created_datetime	
   ,current_datetime as updated_datetime	
